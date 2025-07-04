4347981078be97cf9a6178ad3028b6720e4c3db0
Daniel <35579566+wieserd@users.noreply.github.com>
2025-07-04 22:11:55 +0200

    Update main.ml

--- c_compiler/main.ml
+++ c_compiler/main.ml
@@ -9,10 +9,12 @@ let () =
     Printf.printf "Semantic analysis complete.\n";
   with
   | Failure msg ->
-    Printf.eprintf "Lexing error: %s at line %d, character %d\n" msg
-      lexbuf.lex_curr_p.pos_lnum
-      (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol)
+    let pos = lexbuf.lex_curr_p in
+    Printf.eprintf "Lexing error: %s at line %d, column %d\n" msg
+      pos.pos_lnum
+      (pos.pos_cnum - pos.pos_bol)
   | Grammar.Error ->
-    Printf.eprintf "Parsing error at line %d, character %d\n" 
-      lexbuf.lex_curr_p.pos_lnum
-      (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol)
+    let pos = lexbuf.lex_curr_p in
+    Printf.eprintf "Parsing error at line %d, column %d\n" 
+      pos.pos_lnum
+      (pos.pos_cnum - pos.pos_bol)
