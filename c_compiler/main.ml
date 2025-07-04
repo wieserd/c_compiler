4892f9d488cbfcf5d3add04a1403f2474b78fd75
Daniel <35579566+wieserd@users.noreply.github.com>
2025-07-04 21:44:14 +0200

    Update main.ml

--- c_compiler/main.ml
+++ c_compiler/main.ml
@@ -1,34 +1,20 @@
 open Lexing
-open Tokens
+open Lexer
+open Grammar
 
 let () = 
   let lexbuf = Lexing.from_channel stdin in
-  let rec loop () = 
-    let token = Lexer.token lexbuf in
-    match token with
-    | EOF -> print_endline "EOF"
-    | _ -> 
-      print_endline (Printf.sprintf "Token: %s" (match token with
-                                                  | INT -> "INT"
-                                                  | RETURN -> "RETURN"
-                                                  | INT_LITERAL i -> "INT_LITERAL(" ^ (string_of_int i) ^ ")"
-                                                  | IDENTIFIER s -> "IDENTIFIER(" ^ s ^ ")"
-                                                  | PLUS -> "PLUS"
-                                                  | MINUS -> "MINUS"
-                                                  | STAR -> "STAR"
-                                                  | SLASH -> "SLASH"
-                                                  | LPAREN -> "LPAREN"
-                                                  | RPAREN -> "RPAREN"
-                                                  | LBRACE -> "LBRACE"
-                                                  | RBRACE -> "RBRACE"
-                                                  | SEMICOLON -> "SEMICOLON"
-                                                  | ASSIGN -> "ASSIGN"
-                                                  | EOF -> "EOF"));
-      loop ()
-  in
   try
-    loop ()
-  with Failure msg ->
+    let program = Grammar.program Lexer.token lexbuf in
+    Printf.printf "Parsing successful!\n";
+    (* You can add code here to process the 'program' AST *)
+  with
+  | Failure msg ->
     Printf.eprintf "Lexing error: %s at line %d, character %d\n" msg
       lexbuf.lex_curr_p.pos_lnum
       (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol)
+  | Grammar.Error ->
+    Printf.eprintf "Parsing error at line %d, character %d\n" 
+      lexbuf.lex_curr_p.pos_lnum
+      (lexbuf.lex_curr_p.pos_cnum - lexbuf.lex_curr_p.pos_bol)
+
