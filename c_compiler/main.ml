19b0dece1ff055ad50774573aaaf0dabb9a2acfb
Daniel <35579566+wieserd@users.noreply.github.com>
2025-07-04 21:58:21 +0200

    Update main.ml

--- c_compiler/main.ml
+++ c_compiler/main.ml
@@ -1,10 +1,15 @@
 open Lexing
+open Lexer
+open Grammar
+open Semantics
 
 let () = 
   let lexbuf = Lexing.from_channel stdin in
   try
-    let _program = Grammar.program Lexer.token lexbuf in (* _program to suppress unused variable warning *)
+    let program = Grammar.program Lexer.token lexbuf in
     Printf.printf "Parsing successful!\n";
+    Semantics.analyze_program program; (* Call semantic analysis *)
+    Printf.printf "Semantic analysis complete.\n";
   with
   | Failure msg ->
     Printf.eprintf "Lexing error: %s at line %d, character %d\n" msg
